7262b7747dfe6dc13b27879713765e10b1578c84
Evan Williams <emw236@cornell.edu>
2024-05-28 14:23:32 -0400

    Add parser setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// main.rs
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "frontend/grammar.pest"] // relative path to your grammar file
+struct MyParser;
+
 fn main() {
-    println!("Hello, world!");
+    let successful_parse = MyParser::parse(Rule::number, "12345");
+    match successful_parse {
+        Ok(parsed) => println!("Parsed successfully: {:?}", parsed),
+        Err(e) => println!("Parsing failed: {}", e),
+    }
 }
+
